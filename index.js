e767d268dfc9864fe45ee77dee5dfd346521ed6a
amanullha <aullah060@gmail.com>
2022-04-30 00:31:44 +0600

    project updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
